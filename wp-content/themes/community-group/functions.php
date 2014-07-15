4a2445349f34ef10fa353e6e1bd4e4fface0cdad
Pea <pea@misfist.com>
2014-07-15 16:14:44 -0400

    Commented out unused metabox code.

--- wp-content/themes/community-group/functions.php
+++ wp-content/themes/community-group/functions.php
@@ -4,6 +4,8 @@ Author: Pea, Glocal
 URL: htp://glocal.coop
 */
 
+include_once( 'library/metaboxes/metabox-functions.php' );
+
 $args = array(
 	'flex-width'    => true,
 	'width'         => 960,
@@ -14,7 +16,35 @@ $args = array(
 add_theme_support( 'custom-header', $args );
 
 
+// function be_sample_metaboxes( $meta_boxes ) {
+//     $prefix = '_cmb_'; // Prefix for all fields
+//     $meta_boxes['test_metabox'] = array(
+//         'id' => 'test_metabox',
+//         'title' => 'Test Metabox',
+//         'pages' => array('post'), // post type
+//         'context' => 'normal',
+//         'priority' => 'high',
+//         'show_names' => true, // Show field names on the left
+//         'fields' => array(
+//             array(
+//                 'name' => 'Test Text',
+//                 'desc' => 'field description (optional)',
+//                 'id' => $prefix . 'test_text',
+//                 'type' => 'text'
+//             ),
+//         ),
+//     );
 
+//     return $meta_boxes;
+// }
+// add_filter( 'cmb_meta_boxes', 'be_sample_metaboxes' );
+
+// add_action( 'init', 'be_initialize_cmb_meta_boxes', 9999 );
+// function be_initialize_cmb_meta_boxes() {
+//     if ( !class_exists( 'cmb_Meta_Box' ) ) {
+//         require_once( 'library/metaboxes/init.php' );
+//     }
+// }
 
 
 ?>
