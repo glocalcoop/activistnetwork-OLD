081c401b999c2891c49ca2f88f3fc280903884d7
Pea <pea@misfist.com>
2014-07-16 15:26:19 -0400

    Removed footer widgets.

--- wp-content/themes/community/footer.php
+++ wp-content/themes/community/footer.php
@@ -6,22 +6,6 @@
 						<h2 class="footer-logo"><a class="logo-NYCP" href="http://nycprepared.org">NYC<span>Prepared</span></a><span class="tagline-NYCP"><?php bloginfo('description'); ?></span></h2>
 					</section>
 					
-					<section class="widgets">
-						<?php if ( is_active_sidebar( 'footer1' ) ) : ?>
-							<?php dynamic_sidebar( 'footer1' ); ?>
-						<?php endif; ?>
-						<?php if ( is_active_sidebar( 'footer2' ) ) : ?>
-							<?php dynamic_sidebar( 'footer2' ); ?>
-						<?php endif; ?>
-						<?php if ( is_active_sidebar( 'footer3' ) ) : ?>
-							<?php dynamic_sidebar( 'footer3' ); ?>
-						<?php endif; ?>
-						<?php if ( is_active_sidebar( 'footer4' ) ) : ?>
-							<?php dynamic_sidebar( 'footer4' ); ?>
-						<?php endif; ?>
-					</section>
-					
-
     				<nav class="footer-nav" role="navigation clearfix">
     					<?php bones_footer_links(); ?>
     				</nav>
@@ -36,13 +20,6 @@
 			<footer class="footer-slug">
 			
 			    <div class="inner-footer">
-					<ul class="nav-social" id="social-NYCP">
-					    <li class="email"><a href="mailto:info@nycprepared.org" target="_blank"></a></li>
-					    <li class="github"><a href="https://github.com/NYCPrepared" target="_blank"></a></li>
-					    <li class="twitter"><a href="https://twitter.com/NYCPrepared" target="_blank"></a></li>
-					    <li class="facebook"><a href="https://www.facebook.com/nycprepared" target="_blank"></a></li>
-					    <li class="rss"><a href="/feed" target="_blank"></a></li>
-                    </ul>
 				    <h6 class="copyright">Copyleft <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></h6>
 			    </div>
 			    
