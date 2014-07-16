fb0f1ebde29b7496900821b4529490ae24f3ee4e
Pea <pea@misfist.com>
2014-07-16 14:37:30 -0400

    Testing again.

--- wp-content/themes/community/front-page.php
+++ wp-content/themes/community/front-page.php
@@ -18,7 +18,7 @@
 			<article class="home-intro">
 				<!-- Displays any content entered in the page used as the front page -->
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>
+					<?php the_content(); ?>Testy
 				<?php endwhile; endif; ?>
 			</article>
 			
