76f9e417775cbc0209d3b32d053d378eb2b6e382
Pea <pea@misfist.com>
2014-07-16 15:26:59 -0400

    Removed NYC:P footer links.

--- wp-content/themes/community/footer.php
+++ wp-content/themes/community/footer.php
@@ -6,6 +6,22 @@
 						<h2 class="footer-logo"><a class="logo-NYCP" href="http://nycprepared.org">NYC<span>Prepared</span></a><span class="tagline-NYCP"><?php bloginfo('description'); ?></span></h2>
 					</section>
 					
+					<section class="widgets">
+						<?php if ( is_active_sidebar( 'footer1' ) ) : ?>
+							<?php dynamic_sidebar( 'footer1' ); ?>
+						<?php endif; ?>
+						<?php if ( is_active_sidebar( 'footer2' ) ) : ?>
+							<?php dynamic_sidebar( 'footer2' ); ?>
+						<?php endif; ?>
+						<?php if ( is_active_sidebar( 'footer3' ) ) : ?>
+							<?php dynamic_sidebar( 'footer3' ); ?>
+						<?php endif; ?>
+						<?php if ( is_active_sidebar( 'footer4' ) ) : ?>
+							<?php dynamic_sidebar( 'footer4' ); ?>
+						<?php endif; ?>
+					</section>
+					
+
     				<nav class="footer-nav" role="navigation clearfix">
     					<?php bones_footer_links(); ?>
     				</nav>
