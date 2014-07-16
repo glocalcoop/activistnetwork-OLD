90779e7071dfa3e42c7c04cc3afc253670a30156
Pea <pea@misfist.com>
2014-07-16 14:36:05 -0400

    Removed test.

--- wp-content/themes/community/front-page.php
+++ wp-content/themes/community/front-page.php
@@ -18,7 +18,7 @@
 			<article class="home-intro">
 				<!-- Displays any content entered in the page used as the front page -->
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>I'm a test
+					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
 			</article>
 			
