7d1a9cc2d242537210d71d5a02b30087a4270152
Pea <pea@misfist.com>
2014-07-22 23:02:29 -0400

    Removed unused menus from sub-site template. Added new local site menu.

--- wp-content/themes/community-group/functions.php
+++ wp-content/themes/community-group/functions.php
@@ -15,4 +15,21 @@ add_theme_support( 'custom-header', $args );
 
 
 
+// Remove menu customization on child themes
+
+add_action( 'after_setup_theme', 'remove_theme_customization_community_group', 20 ); 
+
+function remove_theme_customization_community_group() {
+
+    unregister_nav_menu( 'main-nav' );
+	unregister_nav_menu( 'secondary-nav' );
+	unregister_nav_menu( 'utility-nav' );
+	unregister_nav_menu( 'footer-links' );
+	register_nav_menus(
+		array(
+			'site-nav' => __( 'The Main Site Menu', 'community-group' ),
+		)
+	);
+}
+
 ?>
