5c8fcc9bdcac4476d332b4bb49adebcc9e259b4c
Pea <pea@misfist.com>
2014-09-07 19:43:47 -0400

    Cleaned up.

--- wp-content/themes/network-start/page.php
+++ wp-content/themes/network-start/page.php
@@ -1,19 +1,19 @@
 <?php get_template_part( 'partials/header' ); ?>
 
-			<div id="intro" role="intro" class="intro-<?php global $post; echo $post->post_name; ?>">
+	<div id="intro" role="intro" class="intro-<?php global $post; echo $post->post_name; ?>">
 
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'partials/content-page' ); ?>
+		<?php get_template_part( 'partials/content', 'page' ); ?>
 
-				<?php endwhile; ?>
+		<?php endwhile; ?>
 
-				<?php else : ?>
+		<?php else : ?>
 
-					<?php get_template_part( 'partials/content-404' ); ?>
+			<?php get_template_part( 'partials/content', '404' ); ?>
 
-				<?php endif; ?>
+		<?php endif; ?>
 
-			</div>
+	</div>
 			
 <?php get_template_part( 'partials/footer' ); ?>
