2036e2d5a110aa3991238839a9f1f9cf0eaed642
Pea <pea@misfist.com>
2014-10-09 15:04:56 -0400

    Made errors translation ready.

--- wp-content/themes/network-main/partials/error-multisite.php
+++ wp-content/themes/network-main/partials/error-multisite.php
@@ -1,2 +1,2 @@
 <h2 class="module-heading">Sorry</h2>
-<p>This theme is designed for WP multisite installations. Please activate multisite or try a different theme.</p>
+<p><?php _e( 'This theme is designed for WP multisite installations. Please activate multisite or try a different theme.', 'glocal-theme' ); ?></p>
--- wp-content/themes/network-main/partials/error-plugin.php
+++ wp-content/themes/network-main/partials/error-plugin.php
@@ -1,2 +1,2 @@
 <h2 class="module-heading">Sorry</h2>
-<p>This theme requires a plugin to display this content.</p>
+<p><?php _e( 'This theme requires a plugin to display this content.', 'glocal-theme' ); ?></p>
