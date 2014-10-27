a93ad67e9933c26fa6d7590256111e62381bb3fd
Pea <pea@misfist.com>
2014-10-27 19:07:52 -0400

    Customizer tweaks.

--- wp-content/themes/network-main/library/customizer.php
+++ wp-content/themes/network-main/library/customizer.php
@@ -10,21 +10,21 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function glocal_network_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'nav_menu_locations' )->transport = 'postMessage';
-	$wp_customize->get_setting( 'glo_options_home' )->transport = 'postMessage';
-}
-add_action( 'customize_register', 'glocal_network_customize_register' );
+// function glocal_network_customize_register( $wp_customize ) {
+// 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+// 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+// 	$wp_customize->get_setting( 'nav_menu_locations' )->transport = 'postMessage';
+// 	$wp_customize->get_setting( 'glo_options_home' )->transport = 'postMessage';
+// }
+// add_action( 'customize_register', 'glocal_network_customize_register' );
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function glocal_network_customize_preview_js() {
-	wp_enqueue_script( 'glocal_network_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
-}
-add_action( 'customize_preview_init', 'glocal_network_customize_preview_js' );
+// function glocal_network_customize_preview_js() {
+// 	wp_enqueue_script( 'glocal_network_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+// }
+// add_action( 'customize_preview_init', 'glocal_network_customize_preview_js' );
 
 
 
